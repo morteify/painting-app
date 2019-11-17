3ac4ef8f2547685a1d8c782a6ccdadf3145394da
Michał Węglarz <michal.weglarz@protonmail.com>
2019-11-18 00:28:29 +0100

    Add dynamic opacity to brush

--- main.js
+++ main.js
@@ -8,5 +8,18 @@ const ctx = canvasObject.context
 
 const paintingTool = new PaintingTool(canvas, ctx)
 
+const colorPicker = document.querySelector('.colorPicker')
+colorPicker.onchange = event => {
+	paintingTool.setLineProperties({ strokeStyle: event.target.value })
+}
+
+const opacitySlider = document.querySelector('.opacitySlider')
+opacitySlider.onchange = event => {
+	paintingTool.setLineProperties({ strokeStyleOpacity: event.target.value })
+}
 
 
+const sizeSlider = document.querySelector('.sizeSlider')
+sizeSlider.onchange = event => {
+	paintingTool.setLineProperties({ lineWidth: event.target.value })
+}
--- src/PaintingTool.js
+++ src/PaintingTool.js
@@ -7,20 +7,59 @@ export class PaintingTool {
 		canvas.addEventListener('mousedown', this.startDrawing)
 		canvas.addEventListener('mouseup', this.endDrawing)
 		canvas.addEventListener('mousemove', this.moveDrawing, false)
+		this.defaultLineProps = {
+			lineWidth: 30,
+			lineJoin: 'round',
+			lineCap: 'round',
+			strokeStyle: '#000000',			
+		}
+		this.setLineProperties(this.defaultLineProps)
+		// canvas.style.cursor = 'wait'
 	}
 
 
-	setLineProperties = () => {
-		this.context.lineWidth = 20
-		this.context.lineJoin = 'round'
-		this.context.lineCap = 'round'
+	// createSvgShape = () => {
+	// 	const ns = 'http://www.w3.org/2000/svg'
+	// 	const div = document.getElementById('drawing') 
+	// 	const svg = document.createElementNS(ns, 'svg')
+	// 	svg.setAttributeNS(null, 'width', '100%')
+	// 	svg.setAttributeNS(null, 'height', '100%')
+	// 	div.appendChild(svg)
+	// 	const rect = document.createElementNS(ns, 'rect')
+	// 	rect.setAttributeNS(null, 'width', 100)
+	// 	rect.setAttributeNS(null, 'height', 100)
+	// 	rect.setAttributeNS(null, 'fill', '#f06')
+	// 	svg.appendChild(rect)
+
+	// 	return svg
+	// }
+
+	static addOpacity(currentColor, val) {
+		if (currentColor.includes('#'))
+			return `${currentColor}${val.padStart(2, '0')}`
+		if (currentColor.includes('rgba')) {
+			const regExp = /\(([^)]+)\)/g
+			let rgbaValues = regExp.exec(currentColor)[0].split(',')
+			rgbaValues[3] = ` ${val * 0.01})`
+			rgbaValues = rgbaValues.join(',')
+			return `rgba${rgbaValues}`
+		}
+
+	}
+
+	setLineProperties = lineProps => {
+		Object.entries(lineProps).forEach(([key, val]) => {
+			if (JSON.stringify(lineProps).includes('strokeStyleOpacity')) {
+				return this.context.strokeStyle = PaintingTool.addOpacity(this.context.strokeStyle, lineProps.strokeStyleOpacity)
+			}
+			return this.context[key] = val
+		})
 	}
 
 
 	startDrawing = event => {
 		if (event.button === this.MAIN_MOUSE_BUTTON) {
 			this.shouldDraw = true
-			this.setLineProperties()
 			this.context.beginPath()
 			const elementRect = event.target.getBoundingClientRect()
 			this.context.moveTo(event.clientX - elementRect.left, event.clientY - elementRect.top)
