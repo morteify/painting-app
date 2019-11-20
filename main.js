87c3230613e037f3c835863f6095f2d3bec943ee
Michał Węglarz <michal.weglarz@protonmail.com>
2019-11-20 07:00:59 +0100

    Move Image Preview code to separate class and fix contrast/brightness bugs

--- main.js
+++ main.js
@@ -12,40 +12,11 @@ const canvasElement = document.querySelector('canvas')
 const canvasObject = new Canvas(canvasElement)
 const canvas = canvasObject.canvas
 const ctx = canvasObject.context
-ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 const paintingTool = new PaintingTool(canvas, ctx)
-
 const imageInput = document.querySelector('#image')
 const imagePreview = new ImagePreview(canvas, ctx)
-
-
-
 imagePreview.displayImagePreview(imageInput)
-const invertColorsButton = document.querySelector('#invert-colors-button')
-const greyScaleButton = document.querySelector('#greyscale-button')
-const brightnessSlider = document.querySelector('.brightness-slider')
-const contrastSlider = document.querySelector('.contrast-slider')
-
-brightnessSlider.onclick = event => {
-	console.log(event.target.value)
-	const brightness = parseInt(event.target.value)
-	imagePreview.setBrightness(brightness)
-}
-
-contrastSlider.onclick = event => {
-	const contrast = parseInt(event.target.value)
-	imagePreview.setContrast(contrast)
-}
-
-invertColorsButton.onclick = event => {
-	imagePreview.invertColors()
-}
-
-greyScaleButton.onclick = event => {
-	imagePreview.setGrayscale()
-}
-
 
 
 const menus = document.querySelectorAll('.menu')
@@ -66,7 +37,7 @@ function changeActiveTool(menus, activeTool) {
 
 const iconButtons = document.querySelectorAll('.icon')
 iconButtons.forEach(icon => {
-	icon.onclick = event => {
+	icon.onclick = () => {
 		changeActiveTool(menus, icon.id)
 	}
 })
--- src/ImagePreview.js
+++ src/ImagePreview.js
@@ -4,6 +4,28 @@ export class ImagePreview {
 		this.canvas = canvas
 		this.context = context
 		this.imagePreview = new Image()
+		this.setEventListeners()
+	}
+
+	setEventListeners = () => {
+
+		const elements = [
+			{className: '#invert-colors-button', method: () => this.invertColors()},
+			{className: '#greyscale-button', method: () => this.setGrayscale()},
+			{className: '.brightness-slider', method: event => this.setBrightness(parseInt(event, 10))},
+			{className: '.contrast-slider', method: event => this.setContrast(parseInt(event, 10))},
+		]
+
+		elements.forEach(elem => {
+			try {
+				const item = document.querySelector(elem.className)
+				item.onclick = event => {
+					elem.method(event.target.value)
+				}
+			} catch(error) {
+				console.log(error)
+			}
+		})
 	}
 
 	displayImagePreview = imageInput => {
@@ -18,7 +40,7 @@ export class ImagePreview {
 		}
 
 		this.imagePreview.addEventListener('load', e => {
-			this.context.drawImage(this.imagePreview, 0, 0)
+			this.context.drawImage(this.imagePreview, 0, 0, 1920, 1080)
 		})
 	}
 
@@ -37,17 +59,17 @@ export class ImagePreview {
 	setGrayscale = () => {
 		const imageData = this.context.getImageData(0, 0, canvas.width, canvas.height)
 		for (let i = 0; i < imageData.data.length; i += 4) {
-			var avg = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-			imageData.data[i]     = avg; // red
-			imageData.data[i + 1] = avg; // green
-			imageData.data[i + 2] = avg; // blue
+			let average = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
+			imageData.data[i] = average; 
+			imageData.data[i + 1] = average; 
+			imageData.data[i + 2] = average; 
 		  }
 		  this.context.putImageData(imageData, 0, 0);
 	}
 
 	setBrightness = (brightness) => {
 		const imageData = this.context.getImageData(0, 0, canvas.width, canvas.height)
-		for (var i = 0; i < imageData.data.length; i+= 4) {
+		for (let i = 0; i < imageData.data.length; i+= 4) {
 			imageData.data[i] += 255 * (brightness / 100);
 			imageData.data[i+1] += 255 * (brightness / 100);
 			imageData.data[i+2] += 255 * (brightness / 100);
@@ -55,25 +77,25 @@ export class ImagePreview {
 		this.context.putImageData(imageData, 0, 0);
 	}
 
-
-	truncateColor = (value) => {
-		if (value < 0) {
-		  value = 0;
-		} else if (value > 255) {
-		  value = 255;
-		}
-	  
-		return value;
-	  }
-	  
 	setContrast = contrast => {
 		const imageData = this.context.getImageData(0, 0, canvas.width, canvas.height)
 		const factor = (259.0 * (contrast + 255.0)) / (255.0 * (259.0 - contrast));
-	  
+		
+
+		const editColor = value => {
+			if (value < 0) {
+				value = 0;
+			  } else if (value > 255) {
+				value = 255;
+			  }
+			
+			  return value;
+		}
+
 		for (let i = 0; i <imageData.data.length; i+= 4) {
-		 imageData.data[i] = this.truncateColor(factor * (imageData.data[i] - 128.0) + 128.0);
-		 imageData.data[i+1] = this.truncateColor(factor * (imageData.data[i+1] - 128.0) + 128.0);
-		 imageData.data[i+2] = this.truncateColor(factor * (imageData.data[i+2] - 128.0) + 128.0);
+		 imageData.data[i] = editColor(factor * (imageData.data[i] - 128.0) + 128.0);
+		 imageData.data[i+1] = editColor(factor * (imageData.data[i + 1] - 128.0) + 128.0);
+		 imageData.data[i+2] = editColor(factor * (imageData.data[i + 2] - 128.0) + 128.0);
 		}
 		this.context.putImageData(imageData, 0, 0);
 	  }
